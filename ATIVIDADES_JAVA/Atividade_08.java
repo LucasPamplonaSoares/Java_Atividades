aed5ae40522abee4598adb3f6b4dc86317ad9225
Lucas <lucasreitz25@gmail.com>
2022-06-10 16:35:43 -0300

    main

---
+++ ATIVIDADES_JAVA/Atividade_08.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+* 8) Escreva um programa que leia dois números e exiba mensagem informando o
+* valor do maior número e o valor do menor número. Se os dois números forem
+* iguais, o programa deve exibir mensagem informando este fato.
+*******************************************************************************/
+
+package ATIVIDADES_JAVA;
+
+import java.util.Scanner;
+
+public class Atividade_08 {
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int n1, n2;
+
+        System.out.print("Digite o Primeiro Número: ");
+        n1 = teclado.nextInt();
+
+        System.out.print("Digite o Segundo Número: ");
+        n2 = teclado.nextInt();
+
+        if (n1 > n2) {
+            System.out.println("O Primeiro Número é maior que o Segundo Número");
+        }else if(n1 < n2){
+            System.out.println("O Segundo Número é maior que o Primeiro Número");
+        }else{
+            System.out.println("Os dois números sãso iguais");
+        }
+    }
+}
---
+++ ATIVIDADES_JAVA/Atividade_09.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+* 9) Escreva um programa que leia um número inteiro. Verificar por meio de condição se o
+* valor fornecido está na faixa entre 0 (zero) e 9 (nove). Caso o valor fornecido esteja dentro da
+* faixa, apresentar a mensagem “valor válido”. Caso contrário, apresentar a mensagem “valor
+*  inválido”.
+*******************************************************************************/
+
+
+package ATIVIDADES_JAVA;
+
+import java.util.Scanner;
+
+public class Atividade_09 {
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+
+        System.out.print("Digite o Primeiro Número: ");
+        numero = teclado.nextInt();
+
+        if(numero >=0 && numero <= 9){
+            System.out.println("Valor Válido");
+        }else{
+            System.out.println("Valor Inválido");
+        }
+    }
+}
